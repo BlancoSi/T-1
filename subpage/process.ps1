7c819d8087a67b74cf0eaae5a3ed4f2d0049797a
BlancoSi <ftxkd2015@gmail.com>
2019-11-27 12:31:23 +0800

    Update process.ps1

--- subpage/process.ps1
+++ subpage/process.ps1
@@ -1,4 +1,6 @@
 python3 path.py
 python3 refresh.py
-# python3 -m http.server 8000 --bind 127.0.0.1
+
+python3 -m webbrowser http://127.0.0.1:8000
+python3 -m http.server 8000 --bind 127.0.0.1
 # 无法断点续传，请用其他server代替
