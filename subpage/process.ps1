c9f3785f26ed5538f17812d1fd73899ac18ad33f
BlancoSi <44233819+BlancoSi@users.noreply.github.com>
2020-08-05 16:41:19 +0800

    task

--- subpage/process.ps1
+++ subpage/process.ps1
@@ -1,7 +1,9 @@
+# Set-ExecutionPolicy RemoteSigned 允许直接运行本地脚本
+
 python path.py
 python refresh.py
 
-python -m webbrowser http://127.0.0.1:8000
-python -m http.server 8000
+# python -m webbrowser http://127.0.0.1:8000
+# python -m http.server 8000
 # 无法断点续传，请用其他server代替
 # 无法与n2n通用，原因未知
